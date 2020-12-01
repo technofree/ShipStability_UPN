1a6df8d29075e2db6f00cbeddc38a97be3e4d029
vianfitri <viancukiep@gmail.com>
2020-12-01 09:15:42 +0700

    add irSend sendNEC function for infrared sender

--- Remote/Remote.ino
+++ Remote/Remote.ino
@@ -39,18 +39,18 @@ void readKeypad(){
     Serial.println(key);
   }
   
-  if(keyState == 1) Serial.println(179306957, HEX);
-  if(keyState == 2) Serial.println(179307213, HEX);
-  if(keyState == 4) Serial.println(179307725, HEX);
-  if(keyState == 8) Serial.println(179308749, HEX);
-  if(keyState == 16) Serial.println(179310797, HEX);
-  if(keyState == 32) Serial.println(179314893, HEX);
-  if(keyState == 64) Serial.println(179323085, HEX);
-  if(keyState == 128) Serial.println(179339469, HEX);
-  if(keyState == 256) Serial.println(179372237, HEX);
-  if(keyState == 512) Serial.println(179437773, HEX);
-  if(keyState == 1024) Serial.println(179568845, HEX);
-  if(keyState == 2048) Serial.println(179830989, HEX);
+  if(keyState == 1) { irsend.sendNEC(0xAB001CD, 32); } // Serial.println(179306957, HEX);
+  if(keyState == 2) { irsend.sendNEC(0xAB002CD, 32); } // Serial.println(179307213, HEX);
+  if(keyState == 4) { irsend.sendNEC(0xAB004CD, 32); } // Serial.println(179307725, HEX);
+  if(keyState == 8) { irsend.sendNEC(0xAB008CD, 32); } // Serial.println(179308749, HEX);
+  if(keyState == 16) { irsend.sendNEC(0xAB010CD, 32); } // Serial.println(179310797, HEX);
+  if(keyState == 32) { irsend.sendNEC(0xAB020CD, 32); } // Serial.println(179314893, HEX);
+  if(keyState == 64) { irsend.sendNEC(0xAB040CD, 32); } // Serial.println(179323085, HEX);
+  if(keyState == 128) { irsend.sendNEC(0xAB080CD, 32); } // Serial.println(179339469, HEX);
+  if(keyState == 256) { irsend.sendNEC(0xAB100CD, 32); } // Serial.println(179372237, HEX);
+  if(keyState == 512) { irsend.sendNEC(0xAB200CD, 32); } // Serial.println(179437773, HEX);
+  if(keyState == 1024) { irsend.sendNEC(0xAB400CD, 32); } // Serial.println(179568845, HEX);
+  if(keyState == 2048) { irsend.sendNEC(0xAB800CD, 32); } // Serial.println(179830989, HEX);
 }
 
 void keypadEvent(KeypadEvent key){
