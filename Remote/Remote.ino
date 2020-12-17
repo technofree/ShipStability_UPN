2b043be910269526c385ba97777dfa3c705d9b8f
vianfitri <viancukiep@gmail.com>
2020-12-17 15:40:20 +0700

    Modify 'Remote.ino' change key detection continously

--- Remote/Remote.ino
+++ Remote/Remote.ino
@@ -21,7 +21,7 @@ uint8_t rowPins[ROWS] = {24, 34, 32, 28};
 uint8_t colPins[COLS] = {26, 22, 30};
 Keypad kpd = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
-uint16_t keyState = 0;
+char keyState = '\0';
 
 void setup() {
   Serial.begin(9600);
@@ -35,44 +35,44 @@ void loop() {
 void readKeypad(){
   char key = kpd.getKey();
 
-  if(key) {
-    Serial.println(key);
+  if(keyState) {
+    Serial.println(keyState);
   }
   
-  if(keyState == 1) { irsend.sendNEC(0xAB001CD, 32); } // Serial.println(179306957, HEX);
-  if(keyState == 2) { irsend.sendNEC(0xAB002CD, 32); } // Serial.println(179307213, HEX);
-  if(keyState == 4) { irsend.sendNEC(0xAB004CD, 32); } // Serial.println(179307725, HEX);
-  if(keyState == 8) { irsend.sendNEC(0xAB008CD, 32); } // Serial.println(179308749, HEX);
-  if(keyState == 16) { irsend.sendNEC(0xAB010CD, 32); } // Serial.println(179310797, HEX);
-  if(keyState == 32) { irsend.sendNEC(0xAB020CD, 32); } // Serial.println(179314893, HEX);
-  if(keyState == 64) { irsend.sendNEC(0xAB040CD, 32); } // Serial.println(179323085, HEX);
-  if(keyState == 128) { irsend.sendNEC(0xAB080CD, 32); } // Serial.println(179339469, HEX);
-  if(keyState == 256) { irsend.sendNEC(0xAB100CD, 32); } // Serial.println(179372237, HEX);
-  if(keyState == 512) { irsend.sendNEC(0xAB200CD, 32); } // Serial.println(179437773, HEX);
-  if(keyState == 1024) { irsend.sendNEC(0xAB400CD, 32); } // Serial.println(179568845, HEX);
-  if(keyState == 2048) { irsend.sendNEC(0xAB800CD, 32); } // Serial.println(179830989, HEX);
+  if(keyState == '1') { irsend.sendNEC(0xAB001CD, 32); } // Serial.println(179306957, HEX);
+  if(keyState == '2') { irsend.sendNEC(0xAB002CD, 32); } // Serial.println(179307213, HEX);
+  if(keyState == '3') { irsend.sendNEC(0xAB004CD, 32); } // Serial.println(179307725, HEX);
+  if(keyState == '4') { irsend.sendNEC(0xAB008CD, 32); } // Serial.println(179308749, HEX);
+  if(keyState == '5') { irsend.sendNEC(0xAB010CD, 32); } // Serial.println(179310797, HEX);
+  if(keyState == '6') { irsend.sendNEC(0xAB020CD, 32); } // Serial.println(179314893, HEX);
+  if(keyState == '7') { irsend.sendNEC(0xAB040CD, 32); } // Serial.println(179323085, HEX);
+  if(keyState == '8') { irsend.sendNEC(0xAB080CD, 32); } // Serial.println(179339469, HEX);
+  if(keyState == '9') { irsend.sendNEC(0xAB100CD, 32); } // Serial.println(179372237, HEX);
+  if(keyState == '*') { irsend.sendNEC(0xAB200CD, 32); } // Serial.println(179437773, HEX);
+  if(keyState == '0') { irsend.sendNEC(0xAB400CD, 32); } // Serial.println(179568845, HEX);
+  if(keyState == '#') { irsend.sendNEC(0xAB800CD, 32); } // Serial.println(179830989, HEX);
 }
 
 void keypadEvent(KeypadEvent key){
   switch(kpd.getState()){
     case PRESSED:
     case HOLD:
-        if(key == '1') keyState = 1;
-        if(key == '2') keyState = 2;
-        if(key == '3') keyState = 4;
-        if(key == '4') keyState = 8;
-        if(key == '5') keyState = 16;
-        if(key == '6') keyState = 32;
-        if(key == '7') keyState = 64;
-        if(key == '8') keyState = 128;
-        if(key == '9') keyState = 256;
-        if(key == '*') keyState = 512;
-        if(key == '0') keyState = 1024;
-        if(key == '#') keyState = 2048;
+        if(key == '1') keyState = '1';
+        if(key == '2') keyState = '2';
+        if(key == '3') keyState = '3';
+        if(key == '4') keyState = '4';
+        if(key == '5') keyState = '5';
+        if(key == '6') keyState = '6';
+        if(key == '7') keyState = '7';
+        if(key == '8') keyState = '8';
+        if(key == '9') keyState = '9';
+        if(key == '*') keyState = '*';
+        if(key == '0') keyState = '0';
+        if(key == '#') keyState = '#';
         break;
         
     case RELEASED:
-        keyState = 0;
+        keyState = '\0';
         break;
   }
 }
